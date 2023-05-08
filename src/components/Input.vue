990c5adcd8856e28cbcb87158b744dc5842a2661
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-05-08 18:07:34 -0300

    ⚡️ feat: add generic input component

---
+++ src/components/Input.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="input-form">
+        <div class="label-input">
+            <label for="name">{{label}}</label>
+            <span v-if="isObrigatory" class="obrigatory">*</span>
+        </div>
+        <input :type="kind" :name="name" :placeholder="placeholder" />
+    </div>
+</template>
+<script setup lang="ts">
+import { defineProps, onMounted } from 'vue'
+
+const props = defineProps<{
+    label: string
+    isObrigatory: boolean
+    placeholder: string
+    kind: string
+    name: string
+}>()
+
+onMounted(() => {
+  console.log(props)
+})
+</script>
