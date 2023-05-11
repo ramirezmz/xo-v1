f2a036a1ec02ec1c9d7d514434bfa7de1f64baaf
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2023-05-10 23:32:38 -0300

    ⚡️ feat: created login account

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,46 @@
 <template>
-    login
+    <div class="content">
+      <GoBack class="move"/>
+    <h1 class="title">Accessar minha conta</h1>
+      <section class="form">
+        <Input
+            label="Meu email"
+            :is-required="true"
+            placeholder="Digite seu email"
+            kind="input"
+            name="email"
+            v-model="email"
+        />
+        <Input
+            label="Minha senha"
+            :is-required="true"
+            placeholder="Digite sua senha"
+            kind="password"
+            name="password"
+            v-model="password"
+        />
+          <button class="button-option">
+              entrar
+          </button>
+      </section>
+    </div>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import Input from '../components/Input.vue'
+import GoBack from '../components/GoBack.vue'
+
+const email = ref('')
+const password = ref('')
+
+function xxx () {
+  console.log(email.value)
+  console.log(password.value)
+}
+</script>
+<style scoped>
+.move {
+    margin-left: -332px;
+}
+
+</style>
